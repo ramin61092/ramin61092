fffee2e0a38a09c8c3c909b8d4cc9484828602e5
Alireza (Ramin) Delsouz <ramin61092@gmail.com>
2026-03-19 03:14:38 -0400

    Add documentation and usage instructions for script

--- STIGS/WN11-CC-000326.ps1
+++ STIGS/WN11-CC-000326.ps1
@@ -1,9 +1,33 @@
-Enable PowerShell Script Block Logging
+<#
+.SYNOPSIS
+    Enable PowerShell Script Block Logging
+.NOTES
+    Author          : Ramin Delsouz
+    LinkedIn        : linkedin.com/in/alireza-delsouz
+    GitHub          : github.com/ramin61092
+    Date Created    : 2026-03-19
+    Last Modified   : 2026-03-19
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000326
 
+.TESTED ON
+    Date(s) Tested  : 2026-03-19
+    Tested By       : Ramin Delsouz
+    Systems Tested  : 
+    PowerShell Ver. : 5.1.26100.7920
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000326).ps1 
+#> 
+#To enable
 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" -Force | Out-Null
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" `
   -Name "EnableScriptBlockLogging" -PropertyType DWord -Value 1 -Force | Out-Null
 
-Verify
+#Verify
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" `
   -Name "EnableScriptBlockLogging"
