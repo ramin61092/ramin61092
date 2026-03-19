9ace92c40a907b90b1b3b117e72dd48dc0f25d46
Alireza (Ramin) Delsouz <ramin61092@gmail.com>
2026-03-19 02:35:41 -0400

    Update and rename WN11-00-000170 to WN11-00-000170.ps1

---
+++ STIGS/WN11-00-000170.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Ramin Delsouz
+    LinkedIn        : linkedin.com/in/alireza-delsouz
+    GitHub          : github.com/ramin61092
+    Date Created    : 2026-03-18
+    Last Modified   : 2026-03-18
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-00-000170
+
+.TESTED ON
+    Date(s) Tested  : 2026-03-18
+    Tested By       : Ramin Delsouz
+    Systems Tested  : 
+    PowerShell Ver. : 5.1.26100.7920
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#> 
+#To disable
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
+
+#To enable (Give vulnerability)
+To give Vulnerability 
+Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -All -NoRestart
+Restart-Computer
